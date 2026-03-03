a9fae43bfea2544f08f35ee248cbe2fe4bc4e9b7
git-cst <csteenberg@hotmail.co.uk>
2026-03-03 20:51:04 +0100

    Add first sqlc generated code for project

---
+++ database/sql/queries/posts.sql
@@ -0,0 +1,36 @@
+-- name: CreatePost :one
+INSERT INTO posts(
+	created_at,
+	updated_at,
+	title,
+	url,
+	description,
+	published_at,
+	feed_id
+)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7
+)
+RETURNING *;
+
+-- name: GetPostsForUser :many
+SELECT
+	p.title,
+	p.url,
+	p.description,
+	p.published_at
+FROM posts as p
+
+INNER JOIN feeds_users fu
+ON p.feed_id = fu.feed_id
+
+WHERE fu.user_id = $1
+
+ORDER BY p.published_at DESC
+LIMIT $2;
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gator
 
 go 1.25.7
 
-require github.com/lib/pq v1.11.2 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lib/pq v1.11.2 // indirect
+)
